d1c6eaa296cfd6da8f3e4f6f41106bbc3aa0dd9d
yamaguchi teru <yamaguchi.teru@moneyforward.co.jp>
2026-04-22 02:45:59 +0000

    Add signup error messages and integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    return if @user.save
+
+    render 'new', status: :unprocessable_entity
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
