683a83971071cda8c4de0f34e8b02386fcb54ddc
yamaguchi teru <yamaguchi.teru@moneyforward.co.jp>
2026-04-28 08:23:20 +0000

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,16 +6,31 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
+  # 指定のワーカー数でテストを並列実行する
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # test/fixtures/*.ymlのfixtureを全てセットアップする
   fixtures :all
 
   include ApplicationHelper
   
+  # テストユーザーがログイン中の場合にtrueをかえす
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  #テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
