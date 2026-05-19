5b0283bf89eae93cff19765f6057f795de95068a
yamaguchi teru <yamaguchi.teru@moneyforward.co.jp>
2026-05-19 06:32:12 +0000

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,11 +2,11 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
-    #セッションリプレイ攻撃から保護する
+    # セッションリプレイ攻撃から保護する
     session[:session_token] = user.session_token
   end
 
-  #永続的セッションのためにユーザーをデータベースに記憶する
+  # 永続的セッションのためにユーザーをデータベースに記憶する
   def remember(user)
     user.remember
     cookies.permanent.encrypted[:user_id] = user.id
@@ -17,19 +17,17 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      if user && session[:session_token] == user.session_token
-        @current_user = user
-      end
+      @current_user = user if user && session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
 
-  #渡されたユーザーがカレントユーザーであればtrueを返す
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
   def current_user?(user)
     user && user == current_user
   end
@@ -39,7 +37,7 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  #永続的セッションを破壊する
+  # 永続的セッションを破壊する
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -50,10 +48,10 @@ module SessionsHelper
   def log_out
     forget(current_user)
     reset_session
-    @current_user = nil #安全のため
+    @current_user = nil # 安全のため
   end
 
-  #アクセスしようとしたURLを保存する
+  # アクセスしようとしたURLを保存する
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
